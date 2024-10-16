b62c96763130e50f7dd714b314cfb7f6ea52b237
Jake Harvey <122819574+Jake7Harvey@users.noreply.github.com>
2024-10-16 10:08:18 -0500

    Add files via upload

---
+++ report-success.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+    
+    <h1>Report Created</h1>
+    
+     Click <a href="index.html">here</a>.</p> to return to the homepage.
+
+</body>
+</html>
+
+
